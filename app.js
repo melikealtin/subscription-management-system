2351265d5223ca848cd0fe14add17c7ed37b66d0
melikealtin <melikealtin414@gmail.com>
2025-02-27 14:09:09 +0300

    Create user and subscription models

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from "express";
-
 import { PORT } from "./config/env.js";
+import connectToDatabase from "./config/database.js";
 
 import userRouter from "./routes/user-router.js";
 import authRouter from "./routes/auth-router.js";
 import subscriptionRouter from "./routes/subscription-router.js";
 
+connectToDatabase();
+
 const app = express();
+
+app.use(express.json());
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
---
+++ models/user-model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 2,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Please enter a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
