d70ac53240fedabc357ace97164a61d0f4e0b865
melikealtin <melikealtin414@gmail.com>
2025-02-27 14:34:57 +0300

    Add error handling middleware

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 import express from "express";
 import { PORT } from "./config/env.js";
 import connectToDatabase from "./config/database.js";
+import cookieParser from "cookie-parser";
 
 import userRouter from "./routes/user-router.js";
 import authRouter from "./routes/auth-router.js";
 import subscriptionRouter from "./routes/subscription-router.js";
+import errorMiddleware from "./middlewares/error-middleware.js";
 
 connectToDatabase();
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/subscriptions", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Hellooo");
