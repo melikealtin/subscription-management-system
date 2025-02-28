08951cdce1c83cc15bcf1e78785cd4bbaf0d7acd
melikealtin <melikealtin414@gmail.com>
2025-02-28 22:17:19 +0300

    Add arcjet security middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/user-router.js";
 import authRouter from "./routes/auth-router.js";
 import subscriptionRouter from "./routes/subscription-router.js";
 import errorMiddleware from "./middlewares/error-middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet-middleware.js";
 
 connectToDatabase();
 
@@ -15,6 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
